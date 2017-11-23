f5b8030e048e4aa8d25b3b73bb1bdde3d6f80741
Quoc Dung <katobi.taiga@gmail.com>
2017-11-23 13:36:12 +0700

    Dung commit

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ server.listen(process.env.PORT || 3000);
 console.log('server running...');
 console.log(os.hostname());
 io.sockets.on('connection', funtion (socket){
-	//activity code
+	console.log('user connected');
+	socket.on('register',function(name){
+		console.log(name+ 'register');
+		socket.un = name;
+	});
+	socket.on('new messege1', function(mess){
+		console.log('new messege1: '+socket.un + ": "+mess);
+		io.sockets.emit('new mess1',{text: socket.un + " : "+mess});
+	});
+	socket.on('new messege2', function(mess){
+		console.log('new messege2: '+socket.un + ": "+mess);
+		io.sockets.emit('new mess2',{text: socket.un + " : "+mess});
+	});
 });
 
